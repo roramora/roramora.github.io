16121fe86bf80feaefbc07defb1a91d53ad369a8
roramora <142582536+roramora@users.noreply.github.com>
2023-09-19 10:29:28 +0000

    Commit 11

--- script.js
+++ script.js
@@ -9,18 +9,20 @@ function startCountdown(targetDate, elementId) {
       const now = new Date().getTime();
       const targetTime = new Date(targetDate).getTime();
       const distance = targetTime - now;
-      const hours = Math.floor(distance / (1000 * 60 * 60));
+      const days = Math.floor((distance/(1000*60*60))/24);
+      const hours = Math.floor((distance/(1000*60*60))%24);
+      // const hours = Math.floor(distance / (1000 * 60 * 60));
       const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
       // const seconds = Math.floor((distance % (1000 * 60)) / 1000);
   
-      countdown.innerHTML = `${hours}h ${minutes}m`;
+      countdown.innerHTML = `${days}d ${hours}h ${minutes}m`;
     }
     updateCountdown();
-    setInterval(updateCountdown, 5000);
+    setInterval(updateCountdown, 3000);
     }
     
     // Update countdowns
       startCountdown('2024-01-23T23:59:59', 'countdown1');
-      startCountdown('2024-04-05T23:59:59', 'countdown2');
+      startCountdown('2024-03-31T23:59:59', 'countdown2');
       startCountdown('2024-06-03T23:59:59', 'countdown3');
-      startCountdown('2024-05-06T23:59:59', 'countdown4');
+      startCountdown('2024-05-04T23:59:59', 'countdown4');
