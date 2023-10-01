c4ac9aeb1089489d898414b7d1d4bf3237df92a0
Aayushya Lakkadwala <72059036+TheGamingGod85@users.noreply.github.com>
2023-10-01 13:29:57 +0530

    TheGamingGod85's Commit
    Major Changes To Color Scheme, Display Current Time and Show Quotes

--- script.js
+++ script.js
@@ -1,3 +1,48 @@
+const quotesJSON = [
+  '"Believe in yourself and all that you are. Know that there is something inside you that is greater than any obstacle."',
+  '"Success is not final, failure is not fatal: It is the courage to continue that counts."',
+  '"The only way to do great work is to love what you do."',
+  `"Don't watch the clock; do what it does. Keep going."`,
+  '"You are never too old to set another goal or to dream a new dream."',
+  '"You are never too old to set another goal or to dream a new dream."'
+];
+
+// Get the container element where you want to display the quotes
+const quoteContainer = document.getElementById('quote-container');
+
+// Loop through the quotes and create a <p> element for each one
+quotesJSON.forEach(quote => {
+  const listItem = document.createElement('li');
+  listItem.textContent = quote;
+  quoteContainer.appendChild(listItem);
+});
+
+function displayTimeAndDate() {
+  const datetimeElement = document.getElementById('datetime');
+  const now = new Date();
+
+  const options = { 
+      weekday: 'long', 
+      year: 'numeric', 
+      month: 'long', 
+      day: 'numeric', 
+      hour: '2-digit', 
+      minute: '2-digit', 
+      second: '2-digit', 
+      timeZoneName: 'short' 
+  };
+
+  const formattedDatetime = now.toLocaleDateString('en-US', options);
+  datetimeElement.textContent = formattedDatetime;
+}
+
+// Call the function to display the time and date immediately
+displayTimeAndDate();
+
+// Update the time and date every second (1000 milliseconds)
+setInterval(displayTimeAndDate, 1000);
+
+
 function startCountdown(targetDate, elementId) {
     const countdown = document.getElementById(elementId);
   
@@ -13,16 +58,17 @@ function startCountdown(targetDate, elementId) {
       const hours = Math.floor((distance/(1000*60*60))%24);
       // const hours = Math.floor(distance / (1000 * 60 * 60));
       const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-      // const seconds = Math.floor((distance % (1000 * 60)) / 1000);
   
-      countdown.innerHTML = `${days}d ${hours}h ${minutes}m`;
+      countdown.innerHTML = `${days} Days, ${hours} Hours, ${minutes} Minutes`;
     }
     updateCountdown();
     setInterval(updateCountdown, 3000);
     }
+
     
     // Update countdowns
       startCountdown('2024-01-23T23:59:59', 'countdown1');
       startCountdown('2024-03-31T23:59:59', 'countdown2');
       startCountdown('2024-06-03T23:59:59', 'countdown3');
       startCountdown('2024-05-04T23:59:59', 'countdown4');
+
